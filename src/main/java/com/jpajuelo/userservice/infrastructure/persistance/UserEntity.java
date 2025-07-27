9ddfe3eea0aa0b34198ed2faf7bacfb65fa1a134
jpajuelo <jpajuelogibaja@gmail.com>
2025-07-27 11:39:23 -0500

    Update

--- src/main/java/com/jpajuelo/userservice/infrastructure/persistance/UserEntity.java
+++ src/main/java/com/jpajuelo/userservice/infrastructure/persistance/UserEntity.java
@@ -5,9 +5,8 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "User")
 public class UserEntity {
+    //tst
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long UserId;
-
-
 }
